449cc72673700ae619691113db0754c0e47ea857
Evelyn <huangehan@gmail.com>
2024-03-27 22:28:29 +1100

    feature: softpink

--- app/src/main/java/com/example/a5046/ui/theme/Color.kt
+++ app/src/main/java/com/example/a5046/ui/theme/Color.kt
@@ -9,6 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val SoftPink = Color(0xFFBD8B9A)
 
 val LightGreen = Color(211,239,198)
 val SoftGreen = Color(163,185,166)
